8e6bda5c4618c84e2c63b2e546a2df5dd1d1c16a
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-09-28 18:00:04 +0900

    usrsController create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 end
