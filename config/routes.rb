60342c2412be3cb6aba1f72666ce979bd9747494
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-10-01 17:32:00 +0900

    RESTful rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root "static_pages#home"
   get  "/help",    to: "static_pages#help"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
-  get '/signup',    to: "users#new"
+  get '/signup',   to: "users#new"
+  get '/login',    to: "sessions#new"
+  post '/login',   to: "session#create"
+  delete '/logout',to: "session#destroy"
   resources :users
 end
