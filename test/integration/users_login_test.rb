c126171350f69546702f54e2b7c1e4d98442c893
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-10-01 21:04:09 +0900

    login_path test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: @user.email,
+                                          password: "invalid" } }
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
