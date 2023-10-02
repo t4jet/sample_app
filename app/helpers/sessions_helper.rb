ebe4a5d684b6b180aac953e309f495ea1033feaa
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-10-02 10:45:57 +0900

    fix: current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,10 +13,7 @@ module SessionsHelper
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if (user_id = session[:user_id])
-      user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.（コードを書き込む）
-        @current_user = user
-      end
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
