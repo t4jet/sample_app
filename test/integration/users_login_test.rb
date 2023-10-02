9553154690903298e00f865286e0d2072b348c9f
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-10-02 10:33:11 +0900

    cookiesがblankでないことをテストする

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -81,8 +81,7 @@ class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not cookies[:remember_token], assigns(:user).remember_token
-    assert_not_empty cookies[:remember_token]
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
