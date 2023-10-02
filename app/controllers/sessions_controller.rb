75a7e70dbe20e52e6ef359db07ae8d91b525a081
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-10-02 10:18:43 +0900

    session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    ?user = User.find_by(email: params[:session][:email].downcase)
+    if ?user && ?user.authenticate(params[:session][:password])
       reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
+      log_in ?user
+      redirect_to ?user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
