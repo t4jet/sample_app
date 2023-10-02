863ecaafe385af49411e6e22070f4b0d9372d5db
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-10-02 10:19:02 +0900

    remmeber_tokenがない時userを割り当てるテスト

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -81,7 +81,8 @@ class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not cookies[:remember_token].blank?
+    assert_not cookies[:remember_token], assigns(:user).remember_token
+    assert_not_empty cookies[:remember_token]
   end
 
   test "login without remembering" do
