80e23b2cc31a51ae6de3a727de52102e109c7825
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-10-08 12:00:17 +0900

    update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,12 +22,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
+      # 更新に成功した場合を扱う
     else
       render 'edit', status: :unprocessable_entity
     end
   end
-  
+
   private
 
     def user_params
