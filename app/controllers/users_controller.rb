efc5056be2c000d7b30d11bebb2a704dd044ff86
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-10-10 13:22:41 +0900

    adminユーザーのみdestroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  
+  before_action :admin_user,     only: :destroy
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -65,5 +65,8 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 end
